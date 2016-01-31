413263e0c4acbc7d3bb01bb2278e501469313cb8
Souciance <souciance.eqdam.rashti@gmail.com>
2016-01-31 13:33:32 +0100

    Removing previous camel routes and adding new HelloWorld integration.

---
+++ HelloWorld/src/main/java/org/souciance/integration/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.souciance.integration.helloworld;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * A bean which we use in the route
+ */
+public class HelloWorld extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		
+		from("restlet:http://localhost:5000/hello")
+		.log("request received")
+		.setBody().simple("Hello to you too")
+		.end();
+		
+	}
+}
