3a3f10279f3a25dca18f3a1f70b98640f6159cf8
Souciance <souciance.eqdam.rashti@gmail.com>
2016-02-15 01:21:22 +0100

    Adding properties file and renamed routebuilder class

--- MapCsvsToJson/src/test/java/org/souciance/integration/csvstojson/RouteTest.java
+++ MapCsvsToJson/src/test/java/org/souciance/integration/csvstojson/RouteTest.java
@@ -1,11 +1,8 @@
 package org.souciance.integration.csvstojson;
 
-import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 
 import org.junit.Test;
-
-import java.io.File;
 
 public class RouteTest extends CamelBlueprintTestSupport {
 	
@@ -18,13 +15,10 @@ public class RouteTest extends CamelBlueprintTestSupport {
     public void testRoute() throws Exception {
         // the route is timer based, so every 5th second a message is send
         // we should then expect at least one message
-        MockEndpoint mock = getMockEndpoint("mock:result");
-        mock.expectedMinimumMessageCount(1);
+        getMockEndpoint("mock:result").expectedMinimumMessageCount(1);
 
-        File testFile = new File("src/test/resources/input.csv");
-        template.sendBody("direct:start", testFile);
         // assert expectations
-        mock.assertIsSatisfied();
+        assertMockEndpointsSatisfied();
     }
 
 }
