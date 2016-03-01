4507cc11960611aac69362a326fcee0e612a1aa5
SoucianceEqdamRashti <souciance.eqdam.rashti@gmail.com>
2016-03-01 11:08:47 +0100

    Update HelloWorld.java

--- HelloWorld/src/main/java/org/souciance/integration/helloworld/HelloWorld.java
+++ HelloWorld/src/main/java/org/souciance/integration/helloworld/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld extends RouteBuilder {
 		from("restlet:http://localhost:5000/hello")
 		.log("request received")
 		.setBody().simple("Hello to you too")
-		.log("response sent - againddd")
+		.log("response sent - adgainddd")
 		.end();
 		
 	}
