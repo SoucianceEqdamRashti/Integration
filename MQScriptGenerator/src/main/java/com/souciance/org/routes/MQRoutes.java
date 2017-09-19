b9a3b2f416484045ecf9879292b12a8268fb5fcb
Souciance <souciance.eqdam.rashti@gmail.com>
2017-09-19 21:35:16 +0200

    Changed main route class name

--- MQScriptGenerator/src/main/java/com/souciance/org/routes/MQRoutes.java
+++ MQScriptGenerator/src/main/java/com/souciance/org/routes/MQRoutes.java
@@ -1,4 +1,4 @@
-package com.souciance.org;
+package com.souciance.org.routes;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
@@ -10,7 +10,7 @@ import java.net.UnknownHostException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class MyRouteBuilder extends RouteBuilder {
+public class MQRoutes extends RouteBuilder {
 
     public void configure() throws UnknownHostException, InterruptedException {
         String user = System.getProperty("user.name"); //platform independent
@@ -33,10 +33,8 @@ public class MyRouteBuilder extends RouteBuilder {
                         String integration = exchange.getIn().getHeader("Integration").toString();
                         exchange.getIn().setHeader("Author", author.substring(2, author.length() - 2));
                         exchange.getIn().setHeader("Integration", integration.substring(2, integration.length() - 2));
-
                     }
                 })
-
                 .log("${headers.Author}")
                 .log("${headers.Date}")
                 .log("${headers.Integration}")
@@ -44,45 +42,55 @@ public class MyRouteBuilder extends RouteBuilder {
                 .setProperty("FileName", ExpressionBuilder.languageExpression("jsonpath", "$.name"))
                 .setHeader("QueueManager", ExpressionBuilder.languageExpression("jsonpath", "$.name"))
                 .setHeader(Exchange.FILE_NAME, constant(exchangeProperty("FileName").convertToString().append(".mqsc")))
-                .recipientList(constant("direct:testFile, direct:Qlocal"));
-                //.to("direct:testFile")
-                //To get the QLocal array
-                //.setBody(ExpressionBuilder.languageExpression("jsonpath", "$..QLocal"))
-                //.log("QUEUE LOCALS --> ${body}")
-
-                //.to("direct:Qlocal")
-                //To get the QRemote array
-                //.setBody(ExpressionBuilder.languageExpression("jsonpath", "$..QRemote"))
-                //.log("First ${body} \r\n")
-                //.log("Finished!");
+                .recipientList(constant("direct:testFile, direct:Qlocal, direct:QAlias, direct:QRemote"));
 
         from("direct:testFile")
                 .to("freemarker:MQScriptHeader.ftl").setBody(body().append("\r\n"))
-
-
                 .to("file:src/data")
                 .end();
 
-        from("direct:Qlocal")
 
-                .split(ExpressionBuilder.languageExpression("jsonpath", "$..QLocal[*]"))
-                .log("QUEUE LOCALS --> ${body}")
-                .setHeader("QLocalName", ExpressionBuilder.languageExpression("jsonpath", "$..NAME"))
-                .setHeader("QLocalMaxDepth", ExpressionBuilder.languageExpression("jsonpath", "$..MAXDEPTH"))
-                .setHeader("QLocalMaxMsgl", ExpressionBuilder.languageExpression("jsonpath", "$..MAXMSGL"))
+
+        from("direct:QAlias")
+                .split(ExpressionBuilder.languageExpression("jsonpath", "$..QAlias[*]"))
+                .log("QUEUE Alias --> ${body}")
+                .setHeader("NAME", ExpressionBuilder.languageExpression("jsonpath", "$..NAME"))
+                .setHeader("TARGET", ExpressionBuilder.languageExpression("jsonpath", "$..TARGET"))
+                .setHeader("DESCR", ExpressionBuilder.languageExpression("jsonpath", "$..DESCR"))
                 .process(new Processor() {
                     public void process(Exchange exchange) throws Exception {
-                        String name = exchange.getIn().getHeader("QLocalName").toString();
-                        String maxDepth = exchange.getIn().getHeader("QLocalMaxDepth").toString();
-                        String maxMsgl = exchange.getIn().getHeader("QLocalMaxMsgl").toString();
+                        String name = exchange.getIn().getHeader("NAME").toString();
+                        String maxDepth = exchange.getIn().getHeader("TARGET").toString();
+                        String maxMsgl = exchange.getIn().getHeader("DESCR").toString();
                         exchange.getIn().setHeader("NAME", name.substring(2, name.length() - 2));
-                        exchange.getIn().setHeader("MAXDEPTH", maxDepth.substring(2, maxDepth.length() - 2));
-                        exchange.getIn().setHeader("MAXMSGL", maxMsgl.substring(2, maxMsgl.length() - 2));
-                        exchange.getIn().setHeader("QSVCINT", "999999999999999");
-
+                        exchange.getIn().setHeader("TARGET", maxDepth.substring(2, maxDepth.length() - 2));
+                        exchange.getIn().setHeader("DESCR", maxMsgl.substring(2, maxMsgl.length() - 2));
                     }
                 })
-                .to("freemarker:MQQueueLocal.ftl")
+                .to("freemarker:com.souciance.org.routes.MQQueueAlias.ftl")
+                .setBody(body().append("\r\n"))
+                .to("file:src/data?fileExist=Append")
+                .end();
+
+        from("direct:QRemote")
+                .split(ExpressionBuilder.languageExpression("jsonpath", "$..QRemote[*]"))
+                .log("QUEUE LOCALS --> ${body}")
+                .setHeader("NAME", ExpressionBuilder.languageExpression("jsonpath", "$..NAME"))
+                .setHeader("RQMNAME", ExpressionBuilder.languageExpression("jsonpath", "$..RQMNAME"))
+                .setHeader("RNAME", ExpressionBuilder.languageExpression("jsonpath", "$..RNAME"))
+                .process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        String name = exchange.getIn().getHeader("NAME").toString();
+                        String maxDepth = exchange.getIn().getHeader("RQMNAME").toString();
+                        String maxMsgl = exchange.getIn().getHeader("RNAME").toString();
+                        System.out.println (name + " " + maxDepth + " " + maxMsgl);
+                        exchange.getIn().setHeader("NAME", name.substring(2, name.length() - 2));
+                        exchange.getIn().setHeader("RQMNAME", maxDepth.substring(2, maxDepth.length() - 2));
+                        exchange.getIn().setHeader("RNAME", maxMsgl.substring(2, maxMsgl.length() - 2));
+                        exchange.getIn().setHeader("XMITQ", "XMITQ");
+                    }
+                })
+                .to("freemarker:MQQueueRemote.ftl")
                 .setBody(body().append("\r\n"))
                 .to("file:src/data?fileExist=Append")
                 .end();
