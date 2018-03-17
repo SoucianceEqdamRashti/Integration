e11cfb316cad77c005cb8c5c4e650ab0307f8af3
Souciance <souciance.eqdam.rashti@gmail.com>
2018-03-17 15:35:45 +0100

    schema file containing definitions of tables for h2 database

---
+++ artefacts/src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+create table EXECUTION_GROUPS ( executionGroup varchar(100), integrationname varchar(100))
+create table APPLICATIONS ( executionGroup varchar(100), integrationname varchar(100), lastDeployDateTime varchar(100), lastDeployPath varchar(200))
