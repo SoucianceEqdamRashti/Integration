8b2e14ff12d99b4ac664780e8fa6639915fe40f8
Souciance <souciance.eqdam.rashti@gmail.com>
2018-03-17 15:37:16 +0100

    main class where spring boot starts

---
+++ artefacts/src/main/java/org/souciance/iib/SpringBootIIBAdminApp.java
@@ -0,0 +1,16 @@
+package org.souciance.iib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIIBAdminApp {
+
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIIBAdminApp.class, args);
+    }
+
+}
